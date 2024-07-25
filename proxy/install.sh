1e0d8a556cc9a6e5cb35dbd3d453d47e35bb02c6
contact@frames.sh <contact@frames.sh>
2024-07-25 02:52:46 +0200

    Node installation added to install.sh

--- proxy/install.sh
+++ proxy/install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 DIR_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
+# Install node v18.12.1
+curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install nodejs -y
+node -v
+
 cd $DIR_PATH
 sed -e "s/WORKING_DIR/$1/g" "$DIR_PATH/proxy-sample.service" > "$DIR_PATH/proxy.service"
 cp -rf ./proxy.service /etc/systemd/system
