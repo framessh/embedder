9cfbd8d5ba3e3667369f72f971fe3ba6cf937f46
contact@frames.sh <contact@frames.sh>
2024-11-04 17:40:29 +0100

    Corrected image save file name regex test.

--- proxy/index.js
+++ proxy/index.js
@@ -249,7 +249,7 @@ const saveImage = (
   ).write(Buffer.from(imageArrayBuffer));
   if (indexFrame === true) {
     fs.createWriteStream(
-      __dirname + "/index/" + frameUrl.replace(/[^0-9a-zA-Z]/g, "") + ".png"
+      __dirname + "/index/" + frameUrl.replace(/[^0-9a-zA-Z\-]/g, "") + ".png"
     ).write(Buffer.from(imageArrayBuffer));
   }
   return baseUrl + "/public/" + imageId + "." + fileExtension;
